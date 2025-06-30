9e40183a25307b393197a5298b98972e29403ab3
Ahmad Haj Rashid <ahmed.rachid38@gmail.com>
2025-07-01 01:25:50 +0300

    Created CRUD methods and connected them to database

--- server.js
+++ server.js
@@ -7,22 +7,24 @@ var cors = require("cors");
 const app = express();
 
 app.use(cors());
-
+app.use(express.json());
 app.use(express.static("public"));
 
-const axios = require("axios")
+const pg = require('pg')
+// const client = new pg.Client(process.env.DATABASE_URL);
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
+// routing
 const homepage = require('./routes/home')
-const recipes = require("./routes/recipes")
+const recipesRouter = require("./routes/recipes")
 const randomRecipesPage = require('./routes/randomRecipe')
 const favoritesPage = require('./routes/favoritePage')
 const search = require('./routes/search')
 app.use("/",homepage);
-app.use("/recipes",recipes);
+app.use("/recipes",recipesRouter);
 app.use("/recipes",randomRecipesPage)
 app.use("/recipes",favoritesPage)
 app.use("/recipes",search)
-// routing
 
 
 app.use((req, res) => {
@@ -31,6 +33,30 @@ app.use((req, res) => {
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`app listening on port http://localhost:${port}`);
-});
+
+
+// connecting the db
+pool
+  .connect()
+  .then((client) => {
+    return client
+      .query("SELECT current_database(), current_user")
+      .then((res) => {
+        client.release();
+
+        const dbName = res.rows[0].current_database;
+        const dbUser = res.rows[0].current_user;
+
+        console.log(
+          `Connected to PostgreSQL as user '${dbUser}' on database '${dbName}'`
+        );
+
+        console.log(`App listening on port http://localhost:${port}`);
+      });
+  })
+  .then(() => {
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.error("Could not connect to database:", err);
+  });
